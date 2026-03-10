b9722ca2161fb1ee543f8a953cc8e4c55668f932
Chaimae-elyaouti <chaimae.elyaouti.ds@gmai.com>
2026-03-10 04:04:24 +0000

    feat: setup dbt transformation layer and postgres on ubuntu

---
+++ transform_bank_reviews/models/staging/stg_reviews.sql
@@ -0,0 +1,31 @@
+{{ config(materialized='table') }}
+
+
+
+WITH raw_data AS (
+
+    SELECT * FROM public.staging_bank_reviews
+
+)
+
+
+
+SELECT
+
+    review_id,
+
+    UPPER(bank_name) as bank_name,      
+
+    reviewer_name as user_name,
+
+    stars as rating,
+
+    review_text,
+
+    published_date,
+
+    scraped_at
+
+FROM raw_data
+
+WHERE review_text IS NOT NULL AND review_text != 'Pas de texte'
