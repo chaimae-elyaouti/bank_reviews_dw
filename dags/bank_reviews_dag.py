07b169e4a1d57da917b3370578076888fc1bc8e0
chaimae-elyaouti <chaimae.elyaouti.ds@gmail.com>
2026-04-02 01:17:33 +0100

    chore: optimize DAG paths for portability and integrate new pipeline stages

--- dags/bank_reviews_dag.py
+++ dags/bank_reviews_dag.py
@@ -1,13 +1,16 @@
+import os
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
+# Configuration dynamique du chemin (Propre pour GitHub)
+DAG_FOLDER = os.path.dirname(os.path.realpath(__file__))
+PROJECT_PATH = os.path.abspath(os.path.join(DAG_FOLDER, '..'))
+PYTHON_EXEC = "python3" 
+
 default_args = {
     'owner': 'data_engineer',
-    'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -15,22 +18,28 @@ default_args = {
 with DAG(
     'bank_reviews_elt_pipeline',
     default_args=default_args,
-    description='Pipeline ELT pour extraire et charger les avis bancaires marocains',
     schedule_interval='@weekly', 
     catchup=False,
 ) as dag:
-    
-    PROJECT_PATH = "/mnt/c/Users/ThinKPad/bank_reviews_dw"
 
-    
     tache_extraction = BashOperator(
         task_id='extraire_donnees_apify',
-        bash_command=f"cd {PROJECT_PATH} && {PROJECT_PATH}/venv/Scripts/python.exe api_extractor.py",
+        bash_command=f"cd {PROJECT_PATH} && python3 api_extractor.py",
     )
 
     tache_chargement = BashOperator(
         task_id='charger_donnees_postgres',
-        bash_command=f"cd {PROJECT_PATH} && {PROJECT_PATH}/venv/Scripts/python.exe load_raw.py",
+        bash_command=f"cd {PROJECT_PATH} && python3 load_raw.py",
     )
 
-    tache_extraction >> tache_chargement
+    tache_langue = BashOperator(
+        task_id='enrichir_langue',
+        bash_command=f"cd {PROJECT_PATH} && python3 enrich_language.py",
+    )
+
+    tache_dbt = BashOperator(
+        task_id='transformer_dbt',
+        bash_command=f"cd {PROJECT_PATH}/transform_bank_reviews && dbt run",
+    )
+
+    tache_extraction >> tache_chargement >> tache_langue >> tache_dbt
